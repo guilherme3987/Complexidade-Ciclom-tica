63b88543f4f16a9d50aea58ab1de4a04877f6e4c
guilherme3987 <guilhermesmt3987@gmail.com>
2024-08-28 16:57:10 -0300

    update no gráfico de churn

--- Analise/arquivos_mais_alterados/main.py
+++ Analise/arquivos_mais_alterados/main.py
@@ -10,13 +10,23 @@ arquivos = [
     'app/src/main/res/values-ru/strings.xml',
     'app/src/main/res/values-zh-rTW/strings.xml',
     'app/src/main/res/values-fr/strings.xml',
-    'app/src/main/res/values-ar/strings.xml'
+    'app/src/main/res/values-ar/strings.xml',
+    'app/src/main/res/values-zh-rHK/strings.xml',
+    'app/src/main/res/values-es/strings.xml',
+    'app/src/main/res/values-pt-rBR/strings.xml',
+    'app/src/main/res/values-cs/strings.xml',
+    'app/src/main/res/values-ja/strings.xml',
+    'app/src/main/res/values-it/strings.xml',
+    'app/src/main/res/values-hu/strings.xml',
+    'app/src/main/java/org/breezyweather/settings/activities/AboutActivity.kt',
+    'app/src/main/res/values-uk/strings.xml',
+    'app/src/main/AndroidManifest.xml'
 ]
 
-alteracoes = [315, 304, 269, 228, 205, 200, 189, 187, 172]
+alteracoes = [315, 304, 269, 228, 205, 200, 189, 187, 172, 156, 151, 145, 141, 139, 137, 131, 131, 123, 123]
 
 # Criar o gráfico
-plt.figure(figsize=(12, 8))
+plt.figure(figsize=(12, 10))
 plt.barh(arquivos, alteracoes, color='skyblue')
 plt.xlabel('Número de Alterações')
 plt.ylabel('Arquivos')
